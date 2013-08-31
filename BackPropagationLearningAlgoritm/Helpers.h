b310d16a55c17910177c33d8aa50f59fc3c74b52
NekitkoL <nekitko87@gmail.com>
2013-08-31 13:54:57 +0300

    added helpers

---
+++ BackPropagationLearningAlgoritm/Helpers.h
@@ -0,0 +1,12 @@
+#ifndef _HELPERS_H_
+#define _HELPERS_H_
+
+#include <stdlib.h>
+
+#define ALLOC_OBJ(TYPE) (TYPE*)malloc(sizeof(TYPE))
+#define ALLOC_ARRAY(TYPE, LENGTH) (TYPE*)malloc(sizeof(TYPE) * LENGTH)
+
+//Returns a random real number which lies in a range [0:1];
+#define NEXT_DOUBLE() ((double)rand() / (double)RAND_MAX)
+
+#endif
--- BackPropagationLearningAlgoritm/main.c
+++ BackPropagationLearningAlgoritm/main.c
@@ -1,6 +1,7 @@
 #include "StopWatch.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 void main()
 {
-
 }
